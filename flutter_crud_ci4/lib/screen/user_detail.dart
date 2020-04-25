93559cbdf820e0bd6345177e2104777e60dad87b
wildanfuady <wildanfuady@gmail.com>
2020-04-26 00:52:31 +0700

    revisi part 3

--- flutter_crud_ci4/lib/screen/user_detail.dart
+++ flutter_crud_ci4/lib/screen/user_detail.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_ci4/model/user.dart';
-import 'package:flutter_crud_ci4/service/api_service.dart';
 import 'package:flutter_crud_ci4/service/user_service.dart';
 import 'package:flutter_crud_ci4/util/capitalize.dart';
 
 class DetailUser extends StatefulWidget {
 
+  // create variabel id untuk menghandle param yang diberikan (wajib ada @required)
   final int id;
-
+  // constructor
   DetailUser({@required this.id, Key key}):super(key: key);
+
   @override
   _DetailUserState createState() => _DetailUserState();
 }
 
 class _DetailUserState extends State<DetailUser> {
 
+  // create variabel untuk menampung User Api Service
   UserApiService apiService;
+  // create variabel _user untuk menampung model User
   User _user;
 
   @override
   void initState() {
+    // inisialisasi state apiService dengan class UserApiService
     apiService = UserApiService();
     super.initState();
   }
@@ -27,6 +31,7 @@ class _DetailUserState extends State<DetailUser> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // header
       appBar: AppBar(
         title: Text("Detail User"),
         leading: IconButton(
@@ -49,18 +54,21 @@ class _DetailUserState extends State<DetailUser> {
       // body
       body: Center(
         child: FutureBuilder<User>(
+          // panggil function getUserBy(int id)
           future: apiService.getUserBy(widget.id),
           builder: (context, snapshot) {
+            // jika connection none atau data = null
             if (snapshot.connectionState == ConnectionState.none &&
                 snapshot.hasData == null) {
               return LinearProgressIndicator();
+            
+            // jika connection berhasil
             } else if (snapshot.connectionState == ConnectionState.done) {
               
               // tampung data dari server
               _user = snapshot.data;
 
               // jika data ada / tidak null
-
               if(_user.id != 0){
 
                 return ListView(
@@ -95,10 +103,12 @@ class _DetailUserState extends State<DetailUser> {
                     ),
                   ],
                 );
-              // jika data null
+              // jika data null / tidak ada
               } else {
                 return Text("User Not Found");
               }
+            
+            // tampilkan container kosong jika terjadi hal lainnya
             } else {
               return Center(
                 child: Container(),
